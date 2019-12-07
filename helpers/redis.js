09a8dbdd88b7e111794724352a1740e6119e1846
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-07 20:29:30 +0100

    Created the redis connection

---
+++ helpers/redis.js
@@ -0,0 +1,10 @@
+const redis = require('redis');
+
+const { REDIS_URL } = process.env;
+const client = redis.createClient(REDIS_URL);
+
+// eslint-disable-next-line no-console
+client.on('connect', () => console.log('Redis client connected'));
+
+// eslint-disable-next-line no-console
+client.on('error', err => console.error(err));
