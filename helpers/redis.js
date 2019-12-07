35111f30dfc541a6d0e8e0ff7b90cef12908aaae
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-07 20:37:19 +0100

    Added redis getter and setter

--- helpers/redis.js
+++ helpers/redis.js
@@ -8,3 +8,20 @@ client.on('connect', () => console.log('Redis client connected'));
 
 // eslint-disable-next-line no-console
 client.on('error', err => console.error(err));
+
+const set = (key, value) => client.set(key, value, redis.print);
+
+const get = key =>
+  client.get(key, (err, result) => {
+    if (err) {
+      console.error(err);
+      return null;
+    }
+
+    return result;
+  });
+
+module.exports = {
+  set,
+  get,
+};
