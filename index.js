72a9320a6fc01ac78767d33d830ac52357674a95
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-07 19:53:40 +0100

    Connected to the mongo db

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const server = require('./app/server');
+const connect = require('./database/connect');
 
-const PORT = process.env.PORT || 5000;
+const { PORT, DB_URL } = process.env;
 
-server.listen(PORT, () =>
+connect(DB_URL)
+  .then(() =>
+    server.listen(PORT, () =>
+      // eslint-disable-next-line no-console
+      console.log(`:::: Server up and doing at port ${PORT} ::::`),
+    ),
+  )
   // eslint-disable-next-line no-console
-  console.log(`:::: Server up and doing at port ${PORT} ::::`),
-);
+  .catch(err => console.error(err));
