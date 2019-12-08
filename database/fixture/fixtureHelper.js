729f3d4cec51e265381c07d5c717e6f4f4f737b2
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-08 21:09:19 +0100

    Added the find fixture endpoint

--- database/fixture/fixtureHelper.js
+++ database/fixture/fixtureHelper.js
@@ -11,10 +11,13 @@ const removeFixture = id => Fixture.findByIdAndDelete(id).exec();
 const updateFixture = (id, update) =>
   Fixture.findByIdAndUpdate(id, update, { new: true }).exec();
 
+const findFixture = cond => Fixture.find(cond).exec();
+
 module.exports = {
   getFixtureById,
   getFixtures,
   createFixture,
   removeFixture,
   updateFixture,
+  findFixture,
 };
