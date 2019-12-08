cca9c4e4b3daa622db5eb20ccd708a2f34cbfef6
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-08 19:07:26 +0100

    Fixed a bug

--- helpers/authHelpers.js
+++ helpers/authHelpers.js
@@ -2,14 +2,16 @@ const { get } = require('./redis');
 const { verify } = require('./jwtHelpers');
 
 const checkToken = async (req, res, next) => {
-  const token = req.get('Authorization').split(' ')[1];
+  const tok = req.get('Authorization');
 
-  if (!token) {
+  if (!tok) {
     return res.status(401).json({
       status: 401,
       message: 'No token provided, must be set on the Authorization Header',
     });
   }
+
+  const token = tok.split(' ')[1];
 
   try {
     const isAuthorized = await verify(token);
--- team/index.js
+++ team/index.js
@@ -8,9 +8,10 @@ const {
   searchTeam,
 } = require('./teamController');
 const { validateId, validateTeamBody } = require('./teamMiddleware');
+const { checkToken } = require('../helpers/authHelpers');
 const wrapInTryCatch = require('../helpers/wrapInTryCatch');
 
-router.get('/', wrapInTryCatch(getTeams));
+router.get('/', checkToken, wrapInTryCatch(getTeams));
 router.post('/', validateTeamBody, wrapInTryCatch(addTeam));
 router.get('/:id', validateId, wrapInTryCatch(getOneTeam));
 router.delete('/:id', validateId, wrapInTryCatch(deleteTeam));
