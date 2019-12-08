7b9f0236d3e227feb7d8e51e3205b253686ca8ab
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-08 16:47:08 +0100

    Completed the login endpoint

--- auth/authControllers.js
+++ auth/authControllers.js
@@ -1,5 +1,6 @@
-const { createUser } = require('../database/user/userHelper');
-const { hash } = require('../helpers/bcryptHelper');
+const { createUser, findUser } = require('../database/user/userHelper');
+const { hash, compare } = require('../helpers/bcryptHelper');
+const { sign } = require('../helpers/jwtHelpers');
 
 const register = async (req, res) => {
   const hashed = await hash(req.body.password);
@@ -12,6 +13,40 @@ const register = async (req, res) => {
   });
 };
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await findUser({ email });
+
+  if (!user) {
+    return res.status(401).json({
+      status: 401,
+      message: 'Incorrect email.',
+    });
+  }
+
+  const match = await compare(password, user.password);
+
+  if (!match) {
+    return res.status(401).json({
+      status: 401,
+      message: 'Incorrect password.',
+    });
+  }
+
+  const token = await sign({
+    id: user.id,
+    idAdmin: user.isAdmin,
+  });
+
+  return res.status(200).json({
+    status: 200,
+    message: 'Signed in successfully.',
+    token,
+  });
+};
+
 module.exports = {
   register,
+  login,
 };
--- auth/index.js
+++ auth/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const { register } = require('./authControllers');
+const { register, login } = require('./authControllers');
 const wrapInTryCatch = require('../helpers/wrapInTryCatch');
 
 router.post('/register', wrapInTryCatch(register));
+router.post('/login', wrapInTryCatch(login));
 
 module.exports = router;
