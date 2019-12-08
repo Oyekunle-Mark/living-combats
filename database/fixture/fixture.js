941924a7d104d32655f2a7734059224f8877d7bb
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-08 16:29:39 +0100

    Set createIndex to true to turn off deprecation warning

--- database/fixture/fixture.js
+++ database/fixture/fixture.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+mongoose.set('useCreateIndex', true);
+
 const fixtureSchema = new mongoose.Schema({
   time: {
     type: Date,
--- database/user/user.js
+++ database/user/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+mongoose.set('useCreateIndex', true);
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
