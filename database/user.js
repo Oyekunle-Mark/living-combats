4fed91bd79c4c9fff9c8026ad12f8bab75dbfa0c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-08 14:35:30 +0100

    Created the user schema

---
+++ database/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    unique: false,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
