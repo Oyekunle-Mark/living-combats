9087d798390f3fc03f2d0574459280c9f2ff13a7
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-09 10:12:49 +0100

    Uses the appropriate db url based on the env

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ require('dotenv').config();
 const server = require('./app/server');
 const connect = require('./database/connect');
 
-const { PORT, DB_URL } = process.env;
+const { PORT, DB_ENV } = process.env;
+let DB_URL;
+
+if (DB_ENV === 'testing') {
+  DB_URL = process.env.TEST_DB_URL;
+} else {
+  DB_URL = process.env.DB_URL;
+}
 
 connect(DB_URL)
   .then(() =>
