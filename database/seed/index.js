3407a96e3b58095c6330d23b898da58b6914d270
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-09 16:01:10 +0100

    completed the code

--- database/seed/index.js
+++ database/seed/index.js
@@ -1,1 +1,15 @@
-const users = require()
+const users = require('./users');
+const teams = require('./teams');
+
+const User = require('../user/user');
+const Team = require('../team/team');
+
+const seed = async () => {
+  await User.insertMany(await users);
+  await Team.insertMany(teams);
+
+  // eslint-disable-next-line no-console
+  console.log('Users and teams seeded successfully.');
+};
+
+seed();
