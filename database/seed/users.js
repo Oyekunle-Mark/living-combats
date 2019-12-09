2ffd3419bd5b3a98d9798868be6d218b334d7b32
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-09 15:43:39 +0100

    User seed

---
+++ database/seed/users.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    email: 'func@func.com',
+    password: 'werocktheworld',
+  },
+  {
+    email: 'rainbow@func.com',
+    password: 'swiminmwiththetide',
+    isAdmin: true,
+  },
+  {
+    email: 'john@wick.com',
+    password: 'killemall',
+    isAdmin: true,
+  },
+  {
+    email: 'kimbo@slice.com',
+    password: 'brawlthegods',
+  },
+];
