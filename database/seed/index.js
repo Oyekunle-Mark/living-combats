3b122aea09abfe3fd267161993e7a3d3b40d8d4f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-09 16:03:44 +0100

    Droped the table before seeding

--- database/seed/index.js
+++ database/seed/index.js
@@ -5,7 +5,10 @@ const User = require('../user/user');
 const Team = require('../team/team');
 
 const seed = async () => {
+  await User.collection.drop();
   await User.insertMany(await users);
+
+  await Team.collection.drop();
   await Team.insertMany(teams);
 
   // eslint-disable-next-line no-console
