4411a34ba9f42b31e03008bd07a96836fa9c887b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-09 15:51:26 +0100

    Team seed

---
+++ database/seed/teams.js
@@ -0,0 +1,13 @@
+module.exports = [
+  { name: 'Liverpool FC', location: 'Merseyside' },
+  { name: 'Arsenal FC', location: 'North London' },
+  { name: 'WestHam United FC', location: 'London' },
+  { name: 'Espanyol FC', location: 'Barcelona' },
+  { name: 'FC Barcelona', location: 'Barcelona' },
+  { name: 'Real Madrid', location: 'Madrid' },
+  { name: 'Atletico de Madrid', location: 'Madrid' },
+  { name: 'Valencia', location: 'Valencia' },
+  { name: 'Sevilla', location: 'Seville' },
+  { name: 'Internazionale Milan', location: 'Milan' },
+  { name: 'Roma', location: 'Rome' },
+];
