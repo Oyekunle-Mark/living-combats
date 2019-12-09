3627b477a4c407744826380f22aa746f03454c0e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-09 12:26:26 +0100

    Tested the auth

--- auth/index.spec.js
+++ auth/index.spec.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const request = require('supertest');
 const server = require('../app/server');
 const User = require('../database/user/user');
@@ -11,7 +12,7 @@ beforeAll(async () => {
   User.collection.drop();
 });
 
-describe(`${baseUrl}/register [GET]`, () => {
+describe(`${baseUrl}/register [POST]`, () => {
   it('Registers a user', () =>
     request(server)
       .post(`${baseUrl}/register`)
@@ -23,6 +24,45 @@ describe(`${baseUrl}/register [GET]`, () => {
       .expect(201)
       .then(res => {
         expect(res.body.message).toEqual('User created successfully.');
+      }));
+
+  it('Validates the req body.', () =>
+    request(server)
+      .post(`${baseUrl}/register`)
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .then(res => {
+        expect(res.body.message).toEqual(
+          'Credential must include email and password of type string.',
+        );
+      }));
+});
+
+describe(`${baseUrl}/login [POST]`, () => {
+  it('logs a user in', () =>
+    request(server)
+      .post(`${baseUrl}/login`)
+      .send({
+        email: 'email@email.com',
+        password: 'worst-kept-secret',
+      })
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(res => {
+        expect(res.body.message).toEqual('Signed in successfully.');
+      }));
+
+  it('verifies the email', () =>
+    request(server)
+      .post(`${baseUrl}/login`)
+      .send({
+        email: 'emails@emails.com',
+        password: 'worst-kept-secret',
+      })
+      .expect('Content-Type', /json/)
+      .expect(401)
+      .then(res => {
+        expect(res.body.message).toEqual('Incorrect email.');
       }));
 
   it('Validates the req body.', () =>
