b96318098c96bab736ea9c56e4710b9ca61bb55e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-09 14:50:18 +0100

    Test the hash method in jwt helper

---
+++ helpers/__tests__/jwt.spec.js
@@ -0,0 +1,17 @@
+const { v4 } = require('uuid');
+const { sign, verify } = require('../jwtHelpers');
+
+describe('Signs and verifies token', () => {
+  const obj = {
+    id: v4(),
+    isAdmin: false,
+  };
+  let token;
+
+  it('Signs an object', async () => {
+    token = await sign(obj);
+
+    expect(token).toBeTruthy();
+    expect(typeof token).toEqual('string');
+  });
+});
