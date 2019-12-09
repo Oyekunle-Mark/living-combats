97fc39b79ecb1b2874e66470676eed25b21aba17
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-09 16:15:04 +0100

    Seed users and teams

--- database/seed/index.js
+++ database/seed/index.js
@@ -3,16 +3,30 @@ const teams = require('./teams');
 
 const User = require('../user/user');
 const Team = require('../team/team');
+const Fixture = require('../fixture/fixture');
+
+// eslint-disable-next-line no-console
+const print = str => console.log(`${str} collection seeded successfully`);
 
 const seed = async () => {
   await User.collection.drop();
   await User.insertMany(await users);
+  print('User');
 
   await Team.collection.drop();
-  await Team.insertMany(teams);
 
-  // eslint-disable-next-line no-console
-  console.log('Users and teams seeded successfully.');
+  const teamsId = teams.map(async team => {
+    const t = await Team.create(team);
+
+    return {
+      id: t.id,
+      location: t.location,
+    };
+  });
+
+  print('Team');
+
+  print('3');
 };
 
 seed();
