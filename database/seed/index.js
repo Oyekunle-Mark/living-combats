3779fa51a5fba7f1928fe5d4579aad50392c888b
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-09 16:28:10 +0100

    first pass solution at a seeder

--- database/seed/index.js
+++ database/seed/index.js
@@ -28,8 +28,24 @@ const seed = async () => {
 
   let start = 0;
   let end = 0;
+  const status = ['completed', 'pending'];
 
-  while (start < end) {}
+  await Fixture.collection.drop();
+
+  while (start < end) {
+    // eslint-disable-next-line no-await-in-loop
+    await Fixture({
+      date: new Date(),
+      home: teamsId[start].id,
+      away: teamsId[end].id,
+      venue: teamsId[start].location,
+      link: `${teamsId[start].id}_${teamsId[start].location}`,
+      status: status[Math.floor(Math.random() * 2)],
+    });
+
+    start += 1;
+    end -= 1;
+  }
 
   print('Fixture');
 
